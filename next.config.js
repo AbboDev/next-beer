e1b6f46898231b5efb19db931248de9af83bb80c
AbboDev <abbodev@gmail.com>
2023-01-09 23:34:00 +0100

    perf: made turbopack use watch poll only with docker

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config) => {
+    if (process.env.DOCKER) {
+      config.watchOptions = {
+        poll: 1000,
+        aggregateTimeout: 300,
+      }
+    }
+
+    return config
+  },
 }
 
 module.exports = nextConfig
