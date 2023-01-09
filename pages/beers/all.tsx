5050078c6374d4a039f6c58fd28f917909f74636
AbboDev <abbodev@gmail.com>
2023-01-10 00:04:08 +0100

    feat: add all beers page

---
+++ pages/beers/all.tsx
@@ -0,0 +1,23 @@
+import Head from 'next/head'
+import Link from 'next/link'
+import styles from '../../styles/Home.module.css'
+
+export default function All() {
+  return (
+    <>
+      <Head>
+        <title>Tutte le birre - Next Beer</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className={styles.main}>
+        <div className={styles.description}>
+          <p>Tutte le birre</p>
+          <div>
+            <Link href="/">Torna alla home</Link>
+          </div>
+        </div>
+      </main>
+    </>
+  )
+}
