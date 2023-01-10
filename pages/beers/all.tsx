8d59693d6dc000e6f58feb4a957a565bbe1a2dd4
AbboDev <abbodev@gmail.com>
2023-01-10 22:05:13 +0100

    feat: add layout to pages while clean them

--- pages/beers/all.tsx
+++ pages/beers/all.tsx
@@ -1,23 +1,12 @@
-import Head from 'next/head'
 import Link from 'next/link'
-import styles from '@/styles/Home.module.css'
+import Layout from '../../components/layout'
 
 export default function All() {
   return (
-    <>
-      <Head>
-        <title>Tutte le birre - Next Beer</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <main className={styles.main}>
-        <div className={styles.description}>
-          <p>Tutte le birre</p>
-          <div>
-            <Link href="/">Torna alla home</Link>
-          </div>
-        </div>
-      </main>
-    </>
+    <Layout title="Tutte le birre">
+      <p>Tutte le birre</p>
+
+      <Link href="/">Torna alla home</Link>
+    </Layout>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,72 +1,17 @@
-import Head from 'next/head'
-import Image from 'next/image'
 import Link from 'next/link'
-import { Inter } from '@next/font/google'
-import styles from '@/styles/Home.module.css'
-
-const inter = Inter({ subsets: ['latin'] })
+import Layout from '@/components/layout'
 
 export default function Home() {
   return (
-    <>
-      <Head>
-        <title>Next Beer</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <main className={styles.main}>
-        <div className={styles.description}>
-          <p>Homepage</p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
-          </div>
-        </div>
+    <Layout title="Homepage">
+      <p>Homepage</p>
 
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-          <div className={styles.thirteen}>
-            <Image
-              src="/thirteen.svg"
-              alt="13"
-              width={40}
-              height={31}
-              priority
-            />
-          </div>
-        </div>
-
-        <div className={styles.grid}>
-          <Link href="/beers/all" className={styles.card}>
-            <h2 className={inter.className}>
-              Tutte le birre <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Mostra la lista con tutte le birre Punk!
-            </p>
-          </Link>
-        </div>
-      </main>
-    </>
+      <Link href="/beers/all">
+        <h2>
+          Tutte le birre <span>-&gt;</span>
+        </h2>
+        <p>Mostra la lista con tutte le birre Punk!</p>
+      </Link>
+    </Layout>
   )
 }
