0aa20044a53ad3e0818de437e753f4583ebafe0a
AbboDev <abbodev@gmail.com>
2023-01-10 23:42:50 +0100

    feat: fetch beers and show them into raw list

--- pages/beers/all.tsx
+++ pages/beers/all.tsx
@@ -1,12 +1,40 @@
+import Image from 'next/image'
 import Link from 'next/link'
-import Layout from '../../components/layout'
+import Layout from '@/components/layout'
+import { GetStaticProps } from 'next'
 
-export default function All() {
+type Props = {
+  beers: Beer[]
+}
+
+export default function All({ beers }: Props) {
   return (
     <Layout title="Tutte le birre">
       <p>Tutte le birre</p>
 
+      <ul style={{ textAlign: 'center', listStyle: 'none' }}>
+        {beers.map(({ id, name, tagline, image_url }) => (
+          <li key={id} style={{ margin: '0 auto 1rem' }}>
+            <Image src={image_url} alt={name} width={39} height={150} />
+            <h3>{name}</h3>
+            <h4>{tagline}</h4>
+          </li>
+        ))}
+      </ul>
+
       <Link href="/">Torna alla home</Link>
     </Layout>
   )
+}
+
+export const getStaticProps: GetStaticProps<Props> = async () => {
+  const beers: Beer[] = await fetch('https://api.punkapi.com/v2/beers').then(
+    (response) => response.json()
+  )
+
+  return {
+    props: {
+      beers,
+    },
+  }
 }
