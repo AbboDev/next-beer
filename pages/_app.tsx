a75154fe75efe043bf47807059b9b7ff63010c5b
AbboDev <abbodev@gmail.com>
2023-01-12 23:05:47 +0100

    feat: setup font family

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,17 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Barlow as Inter } from '@next/font/google'
+
+const inter = Inter({
+  weight: '500',
+  variable: '--barlow-font',
+  subsets: ['latin'],
+})
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className={inter.className}>
+      <Component {...pageProps} />
+    </div>
+  )
 }
