f7ad29115c767c17a876e9a0308784d656b55d1b
AbboDev <abbodev@gmail.com>
2023-01-12 23:04:20 +0100

    feat: create generic button component

---
+++ components/button.tsx
@@ -0,0 +1,49 @@
+import { ReactNode } from 'react'
+import Link from 'next/link'
+
+type Props = {
+  children?: ReactNode
+  tag: string
+  href?: string
+  className?: string
+}
+
+export default function Button({
+  children,
+  tag = 'button',
+  href,
+  className,
+}: Props) {
+  const defaultClassName: string[] = [
+    'inline-block',
+    'transition',
+    'duration-200',
+    'py-2',
+    'px-3',
+    'rounded-md',
+    'bg-white',
+    'text-slate-900',
+    'dark:bg-slate-700',
+    'dark:text-slate-200',
+    'hover:-translate-x-1',
+    'hover:-translate-y-1',
+    // 'hover:text-slate-900',
+    'hover:bg-teal-300',
+    // 'hover:dark:text-slate-200',
+    'hover:dark:bg-teal-700',
+  ]
+
+  const buttonClassName: string = [...defaultClassName, className].join(' ')
+
+  if (tag === 'link' && href) {
+    return (
+      <Link className={buttonClassName} href={href}>
+        {children}
+      </Link>
+    )
+  }
+
+  const CustomTag = tag as keyof JSX.IntrinsicElements
+
+  return <CustomTag className={buttonClassName}>{children}</CustomTag>
+}
