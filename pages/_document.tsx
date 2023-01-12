2f4da2ac69f1d93824570b245e851172db3fd745
AbboDev <abbodev@gmail.com>
2023-01-12 23:04:46 +0100

    feat: add standard style to document

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,11 +1,12 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
+  const bodyClassName = `antialiased text-slate-600 dark:text-slate-400 bg-white dark:bg-slate-900`
   return (
     <Html lang="en">
       <Head />
 
-      <body>
+      <body className={bodyClassName}>
         <Main />
         <NextScript />
       </body>
