b5839b1736c242d7d62eb4d309f71ded004b0dfb
AbboDev <abbodev@gmail.com>
2023-01-13 00:50:23 +0100

    feat: add text research with default image

--- pages/beers/all.tsx
+++ pages/beers/all.tsx
@@ -2,13 +2,67 @@ import Image from 'next/image'
 import Layout from '@/components/layout'
 import { GetServerSideProps } from 'next'
 import H1 from '@/components/h1'
+import { useState, ChangeEvent } from 'react'
+import defaultImage from '@/public/default.png'
 
 type Props = {
-  beers: Beer[]
+  initialBeers: Beer[]
 }
 
-export default function All({ beers }: Props) {
-  const evaluateAlcholVolume = (abv: number | null): string => {
+type SearchState = {
+  beerName?: string
+}
+
+type SearchStateQuery = {
+  beer_name?: string
+}
+
+const fetchBeers = function <T>(params?: URLSearchParams): Promise<T> {
+  const url: URL = new URL(
+    `https://api.punkapi.com/v2/beers?${params?.toString()}`
+  )
+
+  return fetch(url).then((response) => {
+    if (!response.ok) {
+      throw new Error(response.statusText)
+    }
+
+    return response.json() as Promise<T>
+  })
+}
+
+export default function All({ initialBeers }: Props) {
+  const [beers, setBeers] = useState(initialBeers)
+
+  const [search, setSearch] = useState<SearchState>({
+    beerName: '',
+  })
+
+  const handleChange = async (event: ChangeEvent<HTMLInputElement>) => {
+    const { target } = event
+    const { name, value } = target
+
+    const newSearch = {
+      ...search,
+      [name]: value,
+    }
+
+    setSearch(newSearch)
+
+    const objectParams: SearchStateQuery = {}
+    if (newSearch.beerName) {
+      objectParams.beer_name = newSearch.beerName
+    }
+
+    const params = new URLSearchParams(objectParams)
+
+    const beers: Beer[] = await fetchBeers(params)
+
+    console.debug(beers)
+    setBeers(beers)
+  }
+
+  const evaluateAlcoholVolume = (abv: number | null): string => {
     if (!abv) {
       return ''
     }
@@ -36,6 +90,29 @@ export default function All({ beers }: Props) {
     <Layout title="Tutte le birre" className="text-center">
       <H1>Tutte le birre</H1>
 
+      <form action="#" method="GET" className="w-full mb-4 text-left">
+        <fieldset className="grid grid-cols-3">
+          <legend className="text-xl text-white block mb-2">
+            Ricerca birra per&hellip;
+          </legend>
+
+          <div className="col-span-3">
+            <label htmlFor="beerName" className="block mb-1">
+              Nome
+            </label>
+            <input
+              type="search"
+              id="beerName"
+              name="beerName"
+              value={search.beerName}
+              onChange={handleChange}
+              placeholder="Prova a digitare Punk IPA, Brewdog, Beer&hellip;"
+              className="rounded bg-white py-2 px-4 w-full"
+            />
+          </div>
+        </fieldset>
+      </form>
+
       <section className="grid grid-cols-5 gap-x-4 gap-y-4">
         {beers.map(({ id, name, tagline, image_url, abv }) => (
           <div
@@ -43,7 +120,7 @@ export default function All({ beers }: Props) {
             className="p-4 rounded-md bg-slate-200 shadow-lg shadow-slate-800"
           >
             <Image
-              src={image_url}
+              src={image_url || defaultImage}
               alt={name}
               width={150}
               height={150}
@@ -51,7 +128,7 @@ export default function All({ beers }: Props) {
             />
             <h3 className="border-t border-slate-300 text-xl">{name}</h3>
             <strong
-              className={`block mt-2 text-3xl ${evaluateAlcholVolume(abv)}`}
+              className={`block mt-2 text-3xl ${evaluateAlcoholVolume(abv)}`}
             >
               {abv?.toFixed(1)}°
             </strong>
@@ -64,15 +141,11 @@ export default function All({ beers }: Props) {
 }
 
 export const getServerSideProps: GetServerSideProps<Props> = async () => {
-  const beers: Beer[] = await fetch('https://api.punkapi.com/v2/beers').then(
-    (response) => response.json()
-  )
-
-  console.debug(beers[0])
+  const beers: Beer[] = await fetchBeers<Beer[]>()
 
   return {
     props: {
-      beers,
+      initialBeers: beers,
     },
   }
 }
