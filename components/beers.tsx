73641b0a4b7803c6b1858989000cc0c01c1a76b2
AbboDev <abbodev@gmail.com>
2023-01-17 22:31:16 +0100

    feat: add spinner as loading

--- components/beers.tsx
+++ components/beers.tsx
@@ -235,7 +235,14 @@ export default function Beers({ beers }: Props) {
       />
 
       {loading ? (
-        <section>Loading...</section>
+        <section className="mt-12 mb-4 px-2">
+          <div className="w-32 h-32 relative mx-auto">
+            <div className="w-32 h-32 rounded-full border-4 bg-transparent border-l-0 animate-spin border-teal-300 border-r-teal-500 border-b-teal-700"></div>
+            <span className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-xl text-white">
+              Loading...
+            </span>
+          </div>
+        </section>
       ) : (
         <BeerList
           beers={beers}
